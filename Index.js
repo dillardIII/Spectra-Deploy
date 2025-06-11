a46d78cb803e3d25a7a678ffb24d344dc06cc84a
dillardIII <32180783+dillardIII@users.noreply.github.com>
2025-06-11 01:08:53 -0500

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Spectra Prime Bridge is Live.");
+});
+
+app.get("/GhostForge", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "ghostforge.html"));
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
